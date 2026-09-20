39a79c38f1d638830acb0b4f72f07afacfa73474
ImBadAtJavaScriptM <matheusboyd04@gmail.com>
2026-09-20 10:00:45 -0700

    Wire ACME handler into public server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -15,6 +15,7 @@ import (
 	"time"
 
 	acmeserver "github.com/ImBadAtJavaScriptM/Shift-My/internal/acme"
+	"github.com/ImBadAtJavaScriptM/Shift-My/internal/acme"
 	"github.com/ImBadAtJavaScriptM/Shift-My/internal/capture"
 	"github.com/ImBadAtJavaScriptM/Shift-My/internal/config"
 	"github.com/ImBadAtJavaScriptM/Shift-My/internal/dashboard"
